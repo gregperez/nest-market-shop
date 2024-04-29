8dada6ae344504eda0d0e497cc5ec8f8f63376a9
Greg Perez <ext_ghperezm@falabella.cl>
2024-04-29 19:37:45 -0400

    primer commit market-shop api. post, get, getBy, delete funcionando

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+[circleci-image]: https://img.shields.io/circleci/build/github/nestjs/nest/master?token=abc123def456
+[circleci-url]: https://circleci.com/gh/nestjs/nest
+
+# Market Shop API
+
+1. Clonar el proyecto
+2. Instalar las dependencias
+```
+yarn install
+```
+3. Clonar el archivo ```.env.template``` y renombralo a ```.env```
+4. Configurar las variables de entorno
+5. Levantar la base de datos
+```
+docker-compose up -d
+```
+6. Levantar el proyecto ```yarn start:dev```
+
---
+++ src/products/products.service.ts
@@ -0,0 +1,80 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { isUUID } from 'class-validator';
+
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  // TODO: pagination pending
+  async findAll() {
+    return this.productRepository.find({});
+  }
+
+  async findOne(term: string) {
+    let product: Product;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    }
+
+    if (!product && typeof term === 'string') {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+
+    if (!product) {
+      throw new NotFoundException(`Product with term ${term} not found`);
+    }
+
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const { affected } = await this.productRepository.delete(id);
+    if (affected === 0) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error occurred, check the logs',
+    );
+  }
+}
