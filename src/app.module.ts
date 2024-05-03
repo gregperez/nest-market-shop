1c13895e98ff0d7d2e6a62a95dd1bb8565fd6d57
Greg Perez <ext_ghperezm@falabella.cl>
2024-05-03 16:34:33 -0400

    adding static products images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
+import { join } from 'path';
+import * as process from 'process';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
-import * as process from 'process';
 
 @Module({
   imports: [
@@ -19,6 +23,9 @@ import * as process from 'process';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     ProductsModule,
     CommonModule,
