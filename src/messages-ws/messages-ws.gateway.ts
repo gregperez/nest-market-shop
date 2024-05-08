1404e08c177ad9c57e18ed7b0ad999459429859e
Greg Perez <ext_ghperezm@falabella.cl>
2024-05-08 15:45:58 -0400

    agregando Websocket

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,26 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { MessagesWsService } from './messages-ws.service';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket) {
+    this.messagesWsService.registerClient(client);
+
+    console.log({ connections: this.messagesWsService.getConnectedClients() });
+  }
+
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client.id);
+
+    console.log({ connections: this.messagesWsService.getConnectedClients() });
+  }
+}
---
+++ src/messages-ws/messages-ws.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+interface ConnectedClient {
+  [id: string]: Socket;
+}
+
+@Injectable()
+export class MessagesWsService {
+  private connectedClients: ConnectedClient = {};
+
+  registerClient(client: Socket) {
+    this.connectedClients[client.id] = client;
+  }
+
+  removeClient(clientId: string) {
+    delete this.connectedClients[clientId];
+  }
+
+  getConnectedClients(): number {
+    return Object.keys(this.connectedClients).length;
+  }
+}
