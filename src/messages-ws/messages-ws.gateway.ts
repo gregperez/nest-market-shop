8a0533b4399513fad4f945244131f48207868e4a
Greg Perez <ext_ghperezm@falabella.cl>
2024-05-08 18:02:00 -0400

    emitir mensajes desde el servidor

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,26 +1,57 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dto/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
 
-    console.log({ connections: this.messagesWsService.getConnectedClients() });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
 
-    console.log({ connections: this.messagesWsService.getConnectedClients() });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  async handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    // Emit the message only to the client that sent it
+    // client.emit('messages-from-server', {
+    //   fullName: 'Soy yo!!',
+    //   message: payload.message || 'No message provided',
+    // });
+
+    // Emit the message to all connected clients, except the one that sent it
+    // client.broadcast.emit('messages-from-server', {
+    //   fullName: 'Someone else',
+    //   message: payload.message || 'No message provided',
+    // });
+
+    this.wss.emit('messages-from-server', {
+      fullName: 'Everyone',
+      message: payload.message || 'No message provided',
+    });
   }
 }
